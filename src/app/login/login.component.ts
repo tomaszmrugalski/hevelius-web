2dafd2145ea7d653acae1015854b4d2ea9ce7d56
Tomek Mrugalski <tomasz@isc.org>
2019-01-30 02:27:51 +0100

    version is now stored in version.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormControl } from '@angular/forms';
 import { MessagesService } from '../messages.service';
+import { Version } from '../../version';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +10,12 @@ import { MessagesService } from '../messages.service';
 })
 export class LoginComponent implements OnInit {
 
+    version: string;
+
   constructor(private messageService: MessagesService) {
+      const v = new Version;
+      this.version = v.version;
+
 /*      this.loginForm = new FormGroup({
           'user': new FormControl(null, Validators.required),
           'pass': new FormControl(null, Validators.minLength(6))
---
+++ src/version.ts
@@ -0,0 +1,4 @@
+export class Version {
+  title = 'hevelius';
+  version = '0.0.1';
+}
