f989489773ac87cc36ff2ba02fd2eb5312c367d6
Tomek Mrugalski <tomasz@isc.org>
2019-02-08 00:58:21 +0100

    Initial sketch of main component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../services/login.service';
+import { MessagesService } from '../services/messages.service';
+import { Hevelius } from '../../hevelius';
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-main',
@@ -6,8 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+    title: string;
+    version: string;
 
-  constructor() { }
+
+  constructor(private msg: MessagesService,
+              private loginService: LoginService,
+              private cfg: Hevelius) {
+       this.version = this.cfg.version;
+       this.title = this.cfg.title;
+
+/*
+       x = localStorage.getItem('currentUser');
+       const user = this.loginService.getUser();
+       if (!user) {
+           this.msg.add('User not logged in!');
+       }*/
+  }
 
   ngOnInit() {
   }
