56dffd324fd7d9f1d4fe28e1e8ddb109d06fe15f
Tomek Mrugalski <tomasz@isc.org>
2019-02-16 22:45:23 +0100

    Component with list of tasks added

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,25 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { LoginService } from '../services/login.service';
 import { MessagesService } from '../services/messages.service';
+import { TasksService } from '../services/tasks.service';
 import { Hevelius } from '../../hevelius';
 import { User } from '../models/user';
+import { Task } from '../models/task';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
+
 export class MainComponent implements OnInit {
     title: string;
     version: string;
 
+    dataSource: TasksService;
+
+    displayedColumns: string[] = ['task_id', 'user_id', 'state', 'object', 'ra', 'decl', 'exposure'];
+
 
   constructor(private msg: MessagesService,
               private loginService: LoginService,
+              private http: HttpClient,
               private cfg: Hevelius) {
        this.version = this.cfg.version;
        this.title = this.cfg.title;
 
+       console.log('MainComponent::ctor: Instantiating TasksService');
+       this.dataSource = new TasksService(this.msg, this.http, this.loginService);
+
+       this.dataSource.loadTasks();
 /*
        x = localStorage.getItem('currentUser');
        const user = this.loginService.getUser();
