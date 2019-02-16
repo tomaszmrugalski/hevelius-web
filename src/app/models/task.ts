840e2dcf0f7ce413c6c0d2ddc3a1bcb164da5670
Tomek Mrugalski <tomasz@isc.org>
2019-02-16 22:43:00 +0100

    Task model added

---
+++ src/app/models/task.ts
@@ -0,0 +1,48 @@
+
+
+export interface Date {
+    year: number;
+    month: number;
+    day: number;
+    hour: number;
+    minute: number;
+    second: number;
+}
+
+export interface Task {
+    task_id: number;
+    user_id: number;
+    // scope_id: number;
+    object: string;
+    ra: number;
+    decl: number;
+    // descr: string;
+    exposure: number;
+    /* filter: string;
+    binning: number;
+    guiding: boolean;
+    dither: boolean;
+    defocus: boolean;
+    calibrate: boolean;
+    solve: boolean;
+    vphot: boolean;
+    other_cmd: string;
+    min_alt: number;
+    moon_distance: number;
+    skip_before: Date;
+    skip_after: Date;
+    min_interval: number;
+    comment: string; */
+    state: number;
+
+    /* imagename: string;
+    created: Date;
+    activated: Date;
+    performed: Date;
+    max_moon_phase: number;
+    max_sun_alt: number;
+    auto_center: boolean;
+    calibrated: boolean;
+    solved: boolean;
+    sent: boolean; */
+}
