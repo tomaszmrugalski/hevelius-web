ddfffd36490c8beb8c59f4185c286336ba8859e4
Tomek Mrugalski <tomek@isc.org>
2023-11-16 22:59:33 +0100

    Fix for NullInjectorError: No provider for HttpClient!

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { LoginComponent } from './login.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +9,7 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MatToolbarModule],
+      imports: [MatToolbarModule, HttpClientTestingModule],
       declarations: [LoginComponent],
     }).compileComponents();
   });
