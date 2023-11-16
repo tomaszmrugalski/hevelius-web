935e086f379b8554ba27e1a5e4ba1ddf87cfca68
Tomek Mrugalski <tomek@isc.org>
2023-11-16 23:12:22 +0100

    Two more tests fixed!

--- src/app/services/tasks.service.spec.ts
+++ src/app/services/tasks.service.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-
 import { TasksService } from './tasks.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('TasksService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule]
+  }));
 
   it('should be created', () => {
     const service: TasksService = TestBed.get(TasksService);
