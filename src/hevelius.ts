0883ae267e306c435cf026ef98d24e91502c7483
Tomek Mrugalski <thomson@klub.com.pl>
2025-03-08 14:14:55 +0700

    Version bumped to 0.2.0

--- src/hevelius.ts
+++ src/hevelius.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 
 export class Hevelius {
     static title = 'Hevelius';
-    static version = '0.1.0';
+    static version = '0.2.0';
 
     // Make sure there is no trailing slash
     static apiUrl = 'http://localhost:5000/api';
