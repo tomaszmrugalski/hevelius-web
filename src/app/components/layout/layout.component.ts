b947f49e5cf9d573474a44d22a59e535779fd194
Tomek Mrugalski <thomson@klub.com.pl>
2025-04-13 03:18:59 -0500

    Small reorg in layout

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,18 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { TaskViewComponent } from '../task-view/task-view.component';
 import { Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
 import { NightPlanService } from '../../services/night-plan.service';
 import { LoginService } from '../../services/login.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css']
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnDestroy {
   headerTitle = 'Hevelius';
+  private subscriptions: Subscription[] = [];
 
   constructor(
     private dialog: MatDialog,
@@ -20,19 +22,35 @@ export class LayoutComponent {
     private nightPlanService: NightPlanService,
     private loginService: LoginService
   ) {
+    this.setupTitleUpdates();
+  }
+
+  private setupTitleUpdates() {
     // Subscribe to router events to update the header title
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
-    ).subscribe(() => {
-      if (this.router.url === '/night-plan') {
-        // Subscribe to task count changes when on night plan page
-        this.nightPlanService.getTaskCount().subscribe(count => {
-          this.headerTitle = `Night plan: ${count} tasks planned`;
-        });
-      } else {
-        this.headerTitle = 'Hevelius';
-      }
-    });
+    this.subscriptions.push(
+      this.router.events.pipe(
+        filter(event => event instanceof NavigationEnd)
+      ).subscribe(() => {
+        this.updateTitle();
+      })
+    );
+  }
+
+  private updateTitle() {
+    if (this.router.url === '/night-plan') {
+      // Subscribe to task count changes when on night plan page
+      const subscription = this.nightPlanService.getTaskCount().subscribe(count => {
+        this.headerTitle = `Night plan: ${count} tasks planned`;
+      });
+      this.subscriptions.push(subscription);
+    } else {
+      this.headerTitle = 'Hevelius';
+    }
+  }
+
+  ngOnDestroy() {
+    // Clean up all subscriptions
+    this.subscriptions.forEach(sub => sub.unsubscribe());
   }
 
   openAddTaskDialog() {
