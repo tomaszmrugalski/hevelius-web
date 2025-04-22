908c6119cdfa84728a4934b809cc66c39a2a3cc2
Tomek Mrugalski <thomson@klub.com.pl>
2025-04-21 22:59:23 -0500

    Object name completion fixed

--- src/app/components/task-view/search-results.component.ts
+++ src/app/components/task-view/search-results.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, NgModule } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CatalogObject } from '../../services/catalogs.service';
 
