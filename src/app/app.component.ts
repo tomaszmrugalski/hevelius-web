cb9f499b9c5ff962719d9e8d643977b2a6e58bcb
Tomek Mrugalski <tomek@isc.org>
2025-05-24 22:05:13 +0200

    AppComponent is now standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    standalone: false
+    standalone: true,
+    imports: [
+        CommonModule,
+        RouterModule,
+        LayoutComponent
+    ]
 })
 export class AppComponent {
   title = 'hevelius';
